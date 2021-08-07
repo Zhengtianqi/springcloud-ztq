251d66d52387c630b3788ee40a74f3ad73d98522
zhengtianqi <270490096@qq.com>
2021-08-07 12:47:58 +0800

    class的加载顺序

---
+++ jvm/src/main/java/com/ztq/clazz/ClazzLoadSequence.java
@@ -0,0 +1,34 @@
+package com.ztq.clazz;
+
+/**
+ * class的加载顺序
+ *
+ * @author zhengtianqi
+ */
+public class ClazzLoadSequence {
+
+    static class T1 {
+        public static T1 t = new T1(); // step1. count = 0
+        static int count = 2; // step3. count = 2
+
+        private T1() {
+            count++; // step2. count = 1
+        }
+    }
+
+    static class T2 {
+        static int count = 2; // step1. count = 2
+        public static T2 t = new T2(); // step2. count = 2
+
+        private T2() {
+            count++; // step3. count = 3
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(T1.count);
+        System.out.println(T2.count);
+    }
+
+}
